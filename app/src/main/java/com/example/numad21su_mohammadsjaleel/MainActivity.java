76bd91f203f36a33bccd67590e3694a577defea5
shayan_jaleel <shayan.jaleel@gmail.com>
2021-05-29 17:03:06 -0400

    Link second activity from the main activity.

--- app/src/main/java/com/example/numad21su_mohammadsjaleel/MainActivity.java
+++ app/src/main/java/com/example/numad21su_mohammadsjaleel/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.numad21su_mohammadsjaleel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -31,5 +32,9 @@ public class MainActivity extends AppCompatActivity {
             textView.setText("");
             btn.setText(getString(R.string.show_info));
         }
+    }
+    public void openA3(View view) {
+        Intent intent = new Intent(this, A3Activity.class);
+        startActivity(intent);
     }
 }
