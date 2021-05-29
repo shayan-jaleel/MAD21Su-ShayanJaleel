92c037a2e886067a6d65fc8345bd70deab8742c8
shayan_jaleel <shayan.jaleel@gmail.com>
2021-05-29 18:20:28 -0400

    Add onClick listener to set textview text according to the button pressed.

--- app/src/main/java/com/example/numad21su_mohammadsjaleel/A3Activity.java
+++ app/src/main/java/com/example/numad21su_mohammadsjaleel/A3Activity.java
@@ -3,12 +3,54 @@ package com.example.numad21su_mohammadsjaleel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class A3Activity extends AppCompatActivity {
+public class A3Activity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a3);
+        if(savedInstanceState != null && savedInstanceState.getString("PressedInfo") != null){
+            String retrievedString = savedInstanceState.getString("PressedInfo");
+            TextView textView = findViewById(R.id.text_pressed);
+            textView.setText(retrievedString);
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        // Save UI state changes to the savedInstanceState.
+        // This bundle will be passed to onCreate if the process is
+        // killed and restarted.
+        TextView textView = findViewById(R.id.text_pressed);
+        savedInstanceState.putString("PressedInfo", textView.getText().toString());
+    }
+
+    public void onClick(View view) {
+        TextView textView = findViewById(R.id.text_pressed);
+        switch (view.getId()) {
+            case R.id.button_a:
+                textView.setText("Pressed A");
+                break;
+            case R.id.button_b:
+                textView.setText("Pressed B");
+                break;
+            case R.id.button_c:
+                textView.setText("Pressed C");
+                break;
+            case R.id.button_d:
+                textView.setText("Pressed D");
+                break;
+            case R.id.button_e:
+                textView.setText("Pressed E");
+                break;
+            case R.id.button_f:
+                textView.setText("Pressed F");
+                break;
+        }
+
     }
 }
